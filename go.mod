77283bc177de3102be537c167991f0bc0f65af5e
joeoakes <jxo19@psu.edu>
2023-08-21 12:22:20 -0400

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module golandScraping
+
+go 1.20
+
+require golang.org/x/net v0.14.0 // indirect
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+)
+
+func main() {
+	url := "https://example.com" // Replace with the URL you want to crawl
+	crawl(url)
+}
+
+func crawl(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Failed to crawl %s: %v\n", url, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Failed to crawl %s: %s\n", url, resp.Status)
+		return
+	}
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Printf("Failed to parse HTML: %v\n", err)
+		return
+	}
+
+	// Find all anchor tags in the HTML
+	var visitNode func(*html.Node)
+	visitNode = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					fmt.Println(attr.Val)
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			visitNode(c)
+		}
+	}
+	visitNode(doc)
+}
